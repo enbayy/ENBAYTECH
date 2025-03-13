62cc84ad3bc4fdb4f0836135a3c534423aa76634
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-03-13 15:42:53 +0300

    edited

--- src/pages/Products/ProductsPage.jsx
+++ src/pages/Products/ProductsPage.jsx
@@ -26,7 +26,7 @@ const ProductPage = () => {
                                 <div className="relative w-full h-[300px] transform-style-3d transition-transform duration-500 group-hover:rotate-y-180 rounded-tl-3xl rounded-br-3xl border border-[#E76F51] dark:border-[#E9C46A]">
                                     <div className="absolute inset-0 flex flex-col items-center justify-center bg-transparent rounded-tl-3xl rounded-br-3xl shadow-md backface-hidden group-hover:opacity-0 group-hover:invisible transition-opacity duration-500 gap-y-4">
                                         <div className="relative">
-                                            <img src={product.imageUrl} alt={product.title} className="w-44 h-44 object-contain relative p-6" />
+                                            <img src={product.imageUrl} alt={product.title} className="w-40 h-40 object-contain relative p-6" />
                                             <div className="absolute inset-0 bg-[#E76F51] rounded-full dark:bg-[#E9C46A] -z-10">
                                             </div>
                                         </div>
