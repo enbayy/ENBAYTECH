9243b20b01a67722d3017859998a2d79c8826b54
Enes Bayar <103318928+enbayy@users.noreply.github.com>
2025-06-04 01:16:56 +0300

    edited

--- src/components/Navbar/NavbarUp.jsx
+++ src/components/Navbar/NavbarUp.jsx
@@ -14,29 +14,29 @@ const NavbarUp = () => {
     return (
         <div className="bg-white dark:bg-[#1a1f1c] flex items-center justify-center py-1">
             <div className="container mx-auto px-4 py-2 max-w-screen-lg">
-                <div className="flex flex-wrap justify-center md:justify-around items-center gap-4 md:gap-8">
+                <div className="flex flex-col items-center justify-center gap-4 md:flex-row md:justify-around md:items-center">
                     <div className="flex items-center gap-2 md:gap-2">
                         <FaPhone size={20} className="text-[#059669] dark:text-[#10b981]" />
                         <div className="flex gap-2 flex-wrap">
-                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold">TELEFON:</p>
-                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold">0552 231 2086</p>
+                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold font-poppins">TELEFON:</p>
+                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold font-poppins">0552 231 2086</p>
                         </div>
                     </div>
                     <div className="flex items-center gap-2 md:gap-2">
                         <FaEnvelope size={20} className="text-[#059669] dark:text-[#10b981]" />
                         <div className="flex gap-2 flex-wrap">
-                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold">E-MAİL:</p>
+                            <p className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold font-poppins">E-MAİL:</p>
                             <a
                                 href="mailto:1enesbayar@gmail.com"
-                                className="text-[#0f172a] dark:text-[#e0f2f1] hover:underline font-semibold"
+                                className="text-[#0f172a] dark:text-[#e0f2f1] hover:underline font-semibold font-poppins"
                             >
                                 info@enbaytech.com
                             </a>
                         </div>
                     </div>
                     {visitCount !== null && (
-                        <div className="text-[#0f172a] dark:text-[#e0f2f1] font-semibold">
-                            Ziyaretçi Sayısı: {visitCount}
+                        <div className="text-[#0f172a] dark:text-[#e0f2f1] font-poppins text-xs">
+                            Ziyaret Sayısı: {visitCount}
                         </div>
                     )}
                 </div>
